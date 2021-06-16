d0b5715fc39f266a1316ad04df729141bd3bb5a9
xiaoxiong <306505224@qq.com>
2021-06-16 17:36:18 +0800

    111

---
+++ samples/xiaox-spring-native/README.md
@@ -0,0 +1,10 @@
+### 打包原生native的shell
+- ./xiaox-spring-native/build.sh
+### 可执行文件
+- ./target/nativespring
+### 启动可执行文件
+- ./target/nativespring
+### 启动截图
+![image](https://note.youdao.com/yws/res/23737/E4C9DEB83E814C7097501A69F11A42DC)
+### 运行截图
+![image](https://note.youdao.com/yws/res/23739/60F794E49E3E42988CEF788CC95F27E5)
--- samples/xiaox-spring-native/src/main/java/com/xiaox/test/nativespring/NativespringApplication.java
+++ samples/xiaox-spring-native/src/main/java/com/xiaox/test/nativespring/NativespringApplication.java
@@ -2,12 +2,23 @@ package com.xiaox.test.nativespring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.AotProxyHint;
+import org.springframework.nativex.hint.ProxyBits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@AotProxyHint(targetClass=com.xiaox.test.nativespring.NativespringApplication.class,proxyFeatures = ProxyBits.IS_STATIC)
 public class NativespringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NativespringApplication.class, args);
 	}
 
+	@GetMapping(value="/helloworld/native")
+	public String demo() {
+		return "hello world xiaox";
+	}
 }
+
